18c74838a1f0bc1c67b3ec08e38dc7ed2913296a
JieunAn21 <jieunan021@gmail.com>
2021-10-24 21:32:46 +0900

    뷰모델 챌린지 완료

--- ViewModel/ViewModelDemo2/app/src/main/java/com/anushka/viewmodeldemo2/MainActivity.kt
+++ ViewModel/ViewModelDemo2/app/src/main/java/com/anushka/viewmodeldemo2/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.anushka.viewmodeldemo2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.anushka.viewmodeldemo2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var dataBinding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        dataBinding.resultTextView.text = viewModel.getTotal().toString()
+
+        dataBinding.insertButton.setOnClickListener {
+            dataBinding.apply {
+                viewModel.setTotal(inputEditText.text.toString().toInt())
+                resultTextView.text = viewModel.getTotal().toString()
+            }
+        }
     }
 }
