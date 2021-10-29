b5625730e9bf367d7ce0a06dea0caca337eca2e8
JieunAn21 <jieunan021@gmail.com>
2021-10-29 21:59:24 +0900

    Navigation 예제 Project SetUp

--- Navigation/NavDemo1/app/src/main/java/com/anjinny/navdemo1/MainActivity.kt
+++ Navigation/NavDemo1/app/src/main/java/com/anjinny/navdemo1/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.anjinny.navdemo1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.anjinny.navdemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
