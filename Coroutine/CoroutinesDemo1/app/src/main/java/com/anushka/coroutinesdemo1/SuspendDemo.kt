b96ee8c516ee0c4d7577f9aa8fb96d6fee110c6a
JieunAn21 <jieunan021@gmail.com>
2021-11-18 12:35:55 +0900

    [Coroutine]Suspending Functions

---
+++ Coroutine/CoroutinesDemo1/app/src/main/java/com/anushka/coroutinesdemo1/SuspendDemo.kt
@@ -0,0 +1,16 @@
+package com.anushka.coroutinesdemo1
+
+class SuspendDemo {
+
+    private fun firstFunction() {
+        //some code
+
+        //private final void firstFunction()
+    }
+
+    private suspend fun secondFunction() {
+        //some code
+
+        //private final Object secondFunction(Continuation $completion) { return Unit.INSTANCE; }
+    }
+}
