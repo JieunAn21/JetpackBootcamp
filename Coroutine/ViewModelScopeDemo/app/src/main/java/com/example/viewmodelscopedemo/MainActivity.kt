acf0aa81b7a428f8da0c4e58d6c8fe578959f0fb
JieunAn21 <jieunan021@gmail.com>
2021-11-18 17:06:10 +0900

    [Coroutine]ViewModelScope 실습

--- Coroutine/ViewModelScopeDemo/app/src/main/java/com/example/viewmodelscopedemo/MainActivity.kt
+++ Coroutine/ViewModelScopeDemo/app/src/main/java/com/example/viewmodelscopedemo/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.viewmodelscopedemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainViewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        mainViewModel.getUserData()
+        mainViewModel.users.observe(this, Observer { myUsers ->
+            myUsers.forEach {
+                Log.i("MyTag", "name is ${it.name}")
+            }
+        })
     }
 }
