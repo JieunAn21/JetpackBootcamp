6d0f5e7c92aabcb48756aed43c59c4eceabd3c98
JieunAn21 <jieunan021@gmail.com>
2021-11-18 10:41:22 +0900

    [Coroutine]challenge1 완료

--- Coroutine/challenge1/app/src/main/java/com/example/challenge1/MainActivity.kt
+++ Coroutine/challenge1/app/src/main/java/com/example/challenge1/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.challenge1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            Log.d("MyTag", "Dispatcher.Main thread : ${Thread.currentThread().name}")
+        }
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.d("MyTag", "Dispatcher.IO thread : ${Thread.currentThread().name}")
+        }
     }
 }
