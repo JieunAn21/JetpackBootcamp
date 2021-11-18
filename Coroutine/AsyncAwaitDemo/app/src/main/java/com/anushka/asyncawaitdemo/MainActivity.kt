d4ddd444154b3f673c69d3a1c884d9938053e963
JieunAn21 <jieunan021@gmail.com>
2021-11-18 13:28:58 +0900

    [Coroutine]Async Await 완료

--- Coroutine/AsyncAwaitDemo/app/src/main/java/com/anushka/asyncawaitdemo/MainActivity.kt
+++ Coroutine/AsyncAwaitDemo/app/src/main/java/com/anushka/asyncawaitdemo/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.anushka.asyncawaitdemo
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -15,32 +18,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(IO).launch {
-            Log.i("MyTag","Calculation started....")
+        CoroutineScope(Main).launch {
+            Log.i("MyTag", "Calculation started....")
 
-            val stock1 = async {
-                getStock1()
-            }
-            val stock2 = async {
-                getStock2()
-            }
+            val stock1 = async(IO) { getStock1() } //10초
+            val stock2 = async(IO) { getStock2() } //8초
+            val total = stock1.await() + stock2.await() //결과값을 얻기 위해 await 사용. 총 10초 걸림
 
-            val total = stock1.await()+stock2.await()
-
-            Log.i("MyTag","Total is $total")
+            Toast.makeText(applicationContext, "Total is $total", Toast.LENGTH_SHORT).show()
         }
-
     }
 }
 
-private suspend fun getStock1() : Int {
+private suspend fun getStock1(): Int {
     delay(10000)
-    Log.i("MyTag"," stock 1 returned ")
+    Log.i("MyTag", " stock 1 returned ")
     return 55000
 }
 
-private suspend fun getStock2() : Int {
+private suspend fun getStock2(): Int {
     delay(8000)
-    Log.i("MyTag"," stock 2 returned ")
+    Log.i("MyTag", " stock 2 returned ")
     return 35000
 }
