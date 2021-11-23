cb6f48068b030596c90b0eb348dea56e213fcd65
JieunAn21 <jieunan021@gmail.com>
2021-11-23 14:00:19 +0900

    [Room]User Input Validations

--- Room/app/src/main/java/com/example/roomdemo/SubscriberViewModel.kt
+++ Room/app/src/main/java/com/example/roomdemo/SubscriberViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.roomdemo
 
+import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -29,23 +30,31 @@ class SubscriberViewModel(private val repository: SubscriberRepository) : ViewMo
     }
 
     fun saveOrUpdate() {
-        if (isUpdateOrDelete) {
-            subscriberToUpdateOrDelete.name = inputName.value!!
-            subscriberToUpdateOrDelete.email = inputEmail.value!!
-            update(subscriberToUpdateOrDelete)
+        if (inputName.value == null) {
+            statusMessage.value = Event("Please enter subscriber's name")
+        } else if (inputEmail.value == null) {
+            statusMessage.value = Event("Please enter subscriber's email")
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(inputEmail!!.value).matches()) {
+            statusMessage.value = Event("Please enter a correct email address")
         } else {
-            val name = inputName.value!!
-            val email = inputEmail.value!!
-            insert(
-                Subscriber(
-                    0,
-                    name,
-                    email
-                )
-            ) //id를 auto incremental value로 설정해놨기 때문에 0을 집어넣으면 알아서 id를 설정한다.
-            //입력필드 초기화
-            inputName.value = ""
-            inputEmail.value = ""
+            if (isUpdateOrDelete) {
+                subscriberToUpdateOrDelete.name = inputName.value!!
+                subscriberToUpdateOrDelete.email = inputEmail.value!!
+                update(subscriberToUpdateOrDelete)
+            } else {
+                val name = inputName.value!!
+                val email = inputEmail.value!!
+                insert(
+                    Subscriber(
+                        0,
+                        name,
+                        email
+                    )
+                ) //id를 auto incremental value로 설정해놨기 때문에 0을 집어넣으면 알아서 id를 설정한다.
+                //입력필드 초기화
+                inputName.value = ""
+                inputEmail.value = ""
+            }
         }
     }
 
