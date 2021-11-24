e9d40badf37ee432871471136deae2f7d330da4b
JieunAn21 <jieunan021@gmail.com>
2021-11-24 13:10:07 +0900

    [Retrofit]POST Requests

--- Retrofit/RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ Retrofit/RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -10,12 +10,19 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var retService: AlbumService
+
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         retService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
-        getRequestWithQueryParameters()
+
+        textView = findViewById(R.id.text_view)
+
+        //getRequestWithQueryParameters()
         //getRequestWithPathParameters()
+        uploadAlbum()
     }
 
     private fun getRequestWithQueryParameters() {
@@ -23,8 +30,6 @@ class MainActivity : AppCompatActivity() {
             val response = retService.getSortedAlbums(3)
             emit(response)
         }
-
-        val textView = findViewById<TextView>(R.id.text_view)
 
         responseLiveData.observe(this, {
             val albumsList = it.body()?.listIterator()
@@ -51,6 +56,24 @@ class MainActivity : AppCompatActivity() {
         pathResponse.observe(this, {
             val title = it.body()?.title
             Toast.makeText(applicationContext, title, Toast.LENGTH_LONG).show()
+        })
+    }
+
+    private fun uploadAlbum() {
+        val album = AlbumsItem(0, "My title", 3) // 여기에 적은 id는 무시되고 서버에서 새로운 id를 할당한다.
+
+        val postResponse: LiveData<Response<AlbumsItem>> = liveData {
+            val response = retService.uploadAlbum(album)
+            emit(response)
+        }
+
+        postResponse.observe(this, {
+            val receivedAlbumsItem = it.body()
+            val result = " " + "Album Title : ${receivedAlbumsItem?.title}" + "\n" +
+                    " " + "Album Id : ${receivedAlbumsItem?.id}" + "\n" +
+                    " " + "User Id : ${receivedAlbumsItem?.userId}" + "\n\n\n"
+
+            textView.text = result
         })
     }
 }
