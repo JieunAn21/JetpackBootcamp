ff9dd06ddcbcde3d28529c43535ed8b651fa1619
JieunAn21 <jieunan021@gmail.com>
2021-11-24 10:51:06 +0900

    [Retrofit]Query & Path Parameters

--- Retrofit/RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ Retrofit/RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -2,9 +2,9 @@ package com.example.retrofitdemo
 
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
 import retrofit2.Response
 
@@ -13,8 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val retService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+
+        //path parameter example
+        val pathResponse: LiveData<Response<AlbumsItem>> = liveData {
+            val response = retService.getAlbum(3)
+            emit(response)
+        }
+
+        pathResponse.observe(this, {
+            val title = it.body()?.title
+            Toast.makeText(applicationContext, title, Toast.LENGTH_LONG).show()
+        })
+
+
         val responseLiveData: LiveData<Response<Albums>> = liveData {
-            val response = retService.getAlbums()
+            val response = retService.getSortedAlbums(3)
             emit(response)
         }
 
