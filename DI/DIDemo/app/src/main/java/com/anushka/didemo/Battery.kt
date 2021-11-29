6e3405772b4732652c2c516f616bdbfc275f7705
JieunAn21 <jieunan021@gmail.com>
2021-11-29 13:19:31 +0900

    [DI]Working With Interfaces

--- DI/DIDemo/app/src/main/java/com/anushka/didemo/Battery.kt
+++ DI/DIDemo/app/src/main/java/com/anushka/didemo/Battery.kt
@@ -1,14 +1,6 @@
 package com.anushka.didemo
 
-import android.util.Log
-import javax.inject.Inject
+interface Battery {
 
-class Battery @Inject constructor() {
-    init {
-        Log.i("MYTAG", "Battery Constructed")
-    }
-
-    fun getPower() {
-        Log.i("MYTAG", "Battery power is available")
-    }
+    fun getPower()
 }
