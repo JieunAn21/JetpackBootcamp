e1b02acdb5b7d585b07c28c34a9126b53d849239
JieunAn21 <jieunan021@gmail.com>
2021-11-29 21:51:33 +0900

    [MVVM1]Create Room Entity

---
+++ MVVM/TMDBClient/app/src/main/java/com/example/tmdbclient/data/db/MovieDao.kt
@@ -0,0 +1,21 @@
+package com.example.tmdbclient.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+import com.example.tmdbclient.data.model.movie.Movie
+
+@Dao
+interface MovieDao {
+
+    @Insert(onConflict = REPLACE)
+    suspend fun saveMovies(movies: List<Movie>)
+
+    @Query("DELETE FROM popular_movies")
+    suspend fun deleteAllMovies()
+
+    @Query("SELECT * FROM popular_movies")
+    fun getMovies(): LiveData<List<Movie>>
+}
