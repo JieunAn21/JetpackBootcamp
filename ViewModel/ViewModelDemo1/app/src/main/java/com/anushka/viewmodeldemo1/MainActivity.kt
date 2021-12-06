ecd2b1eeccd8bce8a3d4d8cc47bccaa7a38bd074
JieunAn21 <jieunan021@gmail.com>
2021-12-06 09:48:06 +0900

    [MVVM2]Create Model & Complete UseCase

--- ViewModel/ViewModelDemo1/app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
+++ ViewModel/ViewModelDemo1/app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.anushka.viewmodeldemo1
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.anushka.viewmodeldemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MainActivityViewModel
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-        binding.lifecycleOwner = this
-        binding.viewModel = viewModel
+        setContentView(R.layout.activity_main)
+
+        val countText = findViewById<TextView>(R.id.count_text)
+        val button = findViewById<TextView>(R.id.button)
+        countText.text = count.toString()
+        button.setOnClickListener {
+            count++
+            countText.text = count.toString()
+        }
     }
 }
