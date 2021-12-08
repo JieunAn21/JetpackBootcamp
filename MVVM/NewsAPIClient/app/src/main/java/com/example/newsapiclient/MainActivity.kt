421dadd8d7a366d0234ecc8faceb510d538d9ab3
JieunAn21 <jieunan021@gmail.com>
2021-12-08 15:06:59 +0900

    [MVVM2]RecyclerView With DiffUtil

--- MVVM/NewsAPIClient/app/src/main/java/com/example/newsapiclient/MainActivity.kt
+++ MVVM/NewsAPIClient/app/src/main/java/com/example/newsapiclient/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.newsapiclient
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapiclient.databinding.ActivityMainBinding
+import com.example.newsapiclient.presentation.viewmodel.NewsViewModel
+import com.example.newsapiclient.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+    lateinit var viewModel: NewsViewModel
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +25,6 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bnvNews.setupWithNavController(navController)
+        viewModel = ViewModelProvider(this, factory).get(NewsViewModel::class.java)
     }
 }
